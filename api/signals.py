33a98a7e430bfd760d49900b124d1fa0f61a1c9e
Georgi Ossipov <georgi.ossipov@gmail.com>
2020-02-11 21:10:19 +0200

    Typo correction

--- api/signals.py
+++ api/signals.py
@@ -7,7 +7,7 @@ def get_host_os_info(sender, instance, **kwargs):
         command = ssh.execute_bash_command(ipv4_address=instance.ipv4_address,
                                            root_user=instance.username,
                                            root_password=password,
-                                           command="get-os-release-infos")
+                                           command="get-os-release-info")
 
         data = command.get("output") if not command.get("error") else command.get("error")
         instance.data = {"os": data}
